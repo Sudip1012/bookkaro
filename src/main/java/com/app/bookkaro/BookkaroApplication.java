665d931a2ab2984f9cf17cbf4da5fe96aa735548
Sudip1012 <78993022+Sudip1012@users.noreply.github.com>
2025-03-01 23:41:10 +0530

    Initial commit

---
+++ src/main/java/com/app/bookkaro/BookkaroApplication.java
@@ -0,0 +1,13 @@
+package com.app.bookkaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookkaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookkaroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/bookkaro/BookkaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bookkaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookkaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
